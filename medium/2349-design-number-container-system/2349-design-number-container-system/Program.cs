d2634bd7a4388e62912a35825bc0baa3c63ef03d
vyacheslav-suvorov <kenjaool1@rambler.ru>
2025-02-16 00:33:08 +0300

    2349: Add optimized find

--- medium/2349-design-number-container-system/2349-design-number-container-system/Program.cs
+++ medium/2349-design-number-container-system/2349-design-number-container-system/Program.cs
@@ -1,37 +1,35 @@
-﻿using System.Runtime.InteropServices;
-
-NumberContainers nc = new NumberContainers();
-nc.Find(10); // There is no index that is filled with number 10. Therefore, we return -1.
-nc.Change(2, 10); // Your container at index 2 will be filled with number 10.
-nc.Change(1, 10); // Your container at index 1 will be filled with number 10.
-nc.Change(3, 10); // Your container at index 3 will be filled with number 10.
-nc.Change(5, 10); // Your container at index 5 will be filled with number 10.
-nc.Find(10); // Number 10 is at the indices 1, 2, 3, and 5. Since the smallest index that is filled with 10 is 1, we return 1.
-nc.Change(1, 20); // Your container at index 1 will be filled with number 20. Note that index 1 was filled with 10 and then replaced with 20. 
-nc.Find(10); // Number 10 is at the indices 2, 3, and 5. The smallest index that is filled with 10 is 2. Therefore, we return 2.
-
-public class NumberContainers
+﻿public class NumberContainers
 {
-    private List<int> numbers;
+    private Dictionary<int, int> _numbers;
     public NumberContainers()
     {
-        numbers = new List<int>();
+        _numbers = new Dictionary<int, int>();
     }
 
     public void Change(int index, int number)
     {
-        var numberSpan = CollectionsMarshal.AsSpan(numbers);
-
-        if (index < 0 || index > numberSpan.Length)
+        if (index < 0)
             return;
-        numberSpan[index] = number;
+        _numbers[index] = number;
     }
 
     public int Find(int number)
     {
-        var numberSpan = CollectionsMarshal.AsSpan(numbers);
-        if (!numberSpan.IsEmpty)
-            return -1;
-        return numberSpan.IndexOf(number);
+        int minIndex = int.MaxValue;
+        bool found = false;
+
+        foreach (var pair in _numbers)
+        {
+            if (pair.Value == number)
+            {
+                if (pair.Key < minIndex)
+                {
+                    minIndex = pair.Key;
+                    found = true;
+                }
+            }
+        }
+
+        return found ? minIndex : -1;
     }
 }
