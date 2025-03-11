f403205fadc90ab51b9b8655e0bea8021348ec1c
vyacheslav-suvorov <kenjaool1@rambler.ru>
2025-03-11 23:09:50 +0300

    2697: Add new algorithm

--- medium/2697-find-punishment-number/2697-find-punishment-number/Program.cs
+++ medium/2697-find-punishment-number/2697-find-punishment-number/Program.cs
@@ -1,6 +1,20 @@
 ﻿public class Solution {
     public int PunishmentNumber(int n)
     {
-        return 0;
+        if (n is < 1 or > 1000)
+            return -1;
+        int res = 0;
+        int m = 1;
+        for (int i = 0; i <= n; i++)
+        {
+            
+        }
+        
+        // Create array[n] with x^n [1, 4, 9, 16...]
+        // Foreach by array where x^n.ToString() if (x^n).Length == 1 - x^n to int.
+        // -> else for (int j = 1; j < x^n.Length; j)
+        // 1 part before j 2 part after j if sum ==  
+
+        return res;
     }
 }
