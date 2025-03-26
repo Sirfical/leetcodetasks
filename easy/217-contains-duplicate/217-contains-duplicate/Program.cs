c7b529a42f3e7dcc10083d509acd54adb53b2ff4
vyacheslav-suvorov <kenjaool1@rambler.ru>
2025-03-26 23:52:59 +0300

    217: Add hashset

---
+++ easy/217-contains-duplicate/217-contains-duplicate/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public bool ContainsDuplicate(int[] nums)
+    {
+        var dict = new Dictionary<int, int>();
+        foreach (var num in nums)
+        {
+            if (!dict.ContainsKey(num))
+                dict.Add(num, 1);
+            else
+                return true;
+        }
+        return false;
+    }
+}
