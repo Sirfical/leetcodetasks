802a426fe70f38dd5b6d17c09cf75bd1533f7657
vyacheslav-suvorov <kenjaool1@rambler.ru>
2025-03-27 00:18:42 +0300

    242: Dictionary solution

---
+++ easy/242-valid-anagram/242-valid-anagram/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+var sol = new Solution();
+var a = sol.IsAnagram("anagram", "nagaram");
+
+public class Solution {
+    public bool IsAnagram(string s, string t)
+    {
+        var dic = new Dictionary<char, int>();
+        foreach (var c in s.Where(c => !dic.TryAdd(c, 1)))
+        {
+            dic[c]++;
+        }
+
+        foreach (var c in t)
+        {
+            if (dic.ContainsKey(c))
+            {
+                if (dic[c] != 1)
+                    dic[c]--;
+                else
+                {
+                    dic.Remove(c);
+                }
+            }
+            else
+                return false;
+        }
+
+        return dic.Count != 0;
+    }
+}
